df03d246bff6152a62a2d2027e1d6b734f1f2d49
Justin Gent <justinbgent@gmail.com>
2019-11-18 12:11:36 -0700

    Add needed methods for ReceiptRepository

--- oauth2/src/main/java/com/lambdaschool/usermodel/repository/ReceiptRepository.java
+++ oauth2/src/main/java/com/lambdaschool/usermodel/repository/ReceiptRepository.java
@@ -3,6 +3,22 @@ package com.lambdaschool.usermodel.repository;
 import com.lambdaschool.usermodel.models.Receipt;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReceiptRepository extends CrudRepository<Receipt, Long> {
-    
+    /*
+    List<Receipt> getReceipts();
+
+    Receipt findReceiptById(long receiptId);
+
+    Receipt addReceipt(Receipt receipt);
+
+    Receipt updateReceipt(long receiptId, Receipt receipt);
+
+    void deleteReceipt(long receiptId);
+     */
+
+    List<Receipt> getAll();
+
+    Receipt findByReceiptid(long receiptId);
 }
