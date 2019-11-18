03b5157cb988a66e0152de6736c8d71ffae5f597
Justin Gent <justinbgent@gmail.com>
2019-11-18 11:58:51 -0700

    Add Receipt controller with full Crud operations

---
+++ oauth2/src/main/java/com/lambdaschool/usermodel/controllers/ReceiptController.java
@@ -0,0 +1,44 @@
+package com.lambdaschool.usermodel.controllers;
+
+import com.lambdaschool.usermodel.models.Receipt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/receipts")
+public class ReceiptController {
+    @Autowired
+    ReceiptService receiptService;
+
+    @GetMapping(value = "/receipts", produces = "application/json")
+    ResponseEntity<?> getReceipts(){
+        return new ResponseEntity<>(receiptService.getReceipts(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/receipt/{receiptid}", produces = "application/json")
+    ResponseEntity<?> findReceiptById(@PathVariable long receiptid){
+        return new ResponseEntity<>(receiptService.findReceiptById(receiptid), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/receipt", consumes = "application/json")
+    ResponseEntity<?> addReceipt(@Valid @RequestBody Receipt receipt){
+        receiptService.addReceipt(receipt);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/receipt/{receiptid}", consumes = "application/json")
+    ResponseEntity<?> updateReceipt(@RequestBody Receipt receipt, @PathVariable long receiptid){
+        receiptService.updateReceipt(receiptid, receipt);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/receipt/delete/{receiptid}")
+    ResponseEntity<?> deleteReceipt(@PathVariable long receiptid){
+        receiptService.deleteReceipt(receiptid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
