e83fb97fc177e95304a0b197b3c68d10614c85ff
Justin Gent <justinbgent@gmail.com>
2019-11-21 12:15:00 -0700

    Add swagger documentation to Receipt Controller

--- oauth2/src/main/java/com/lambdaschool/usermodel/controllers/ReceiptController.java
+++ oauth2/src/main/java/com/lambdaschool/usermodel/controllers/ReceiptController.java
@@ -32,28 +32,47 @@ public class ReceiptController {
     }
 
     //localhost:2019/receipts/receipt/{receiptid}
+    @ApiOperation(value = "Only Returns User Specific receipt by ID", response = Receipt.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Receipt Returned", response = Receipt.class),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @GetMapping(value = "/receipt/{receiptid}", produces = "application/json")
     ResponseEntity<?> findReceiptById(@ApiParam(value = "Receipt ID", required = true, example = "4") @PathVariable long receiptid, Authentication authentication) {
         return new ResponseEntity<>(receiptService.findReceiptById(receiptid, authentication.getName()), HttpStatus.OK);
     }
 
     //localhost:2019/receipts/receipt
+    @ApiOperation(value = "Creates a new receipt tied to user.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Receipt Created")
+    })
     @PostMapping(value = "/receipt", consumes = "application/json")
-    ResponseEntity<?> addReceipt(@Valid @RequestBody Receipt receipt, Authentication authentication) {
+    ResponseEntity<?> addReceipt(@ApiParam(value = "Full Receipt, Image not Required", required = true)@Valid @RequestBody Receipt receipt, Authentication authentication) {
         receiptService.addReceipt(authentication.getName(), receipt);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     //localhost:2019/receipts/receipt/{receiptid}
+    @ApiOperation(value = "Only Updates User Specific receipt by ID and changed fields")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Receipt Updated"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @PutMapping(value = "/receipt/{receiptid}", consumes = "application/json")
-    ResponseEntity<?> updateReceipt(@RequestBody Receipt receipt, @PathVariable long receiptid, Authentication authentication) {
+    ResponseEntity<?> updateReceipt(@ApiParam(value = "Receipt fields needing an update", required = true)@RequestBody Receipt receipt, @ApiParam(value = "Receipt ID", required = true, example = "4")@PathVariable long receiptid, Authentication authentication) {
         receiptService.updateReceipt(receiptid, receipt, authentication.getName());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     //localhost:2019/receipts/receipt/delete/{receiptid}
+    @ApiOperation(value = "Only Deletes User Specific receipt by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Receipt Deleted"),
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
     @DeleteMapping(value = "/receipt/delete/{receiptid}")
-    ResponseEntity<?> deleteReceipt(@PathVariable long receiptid, Authentication authentication) {
+    ResponseEntity<?> deleteReceipt(@ApiParam(value = "Receipt ID", required = true, example = "4")@PathVariable long receiptid, Authentication authentication) {
         receiptService.deleteReceipt(receiptid, authentication.getName());
         return new ResponseEntity<>(HttpStatus.OK);
     }
